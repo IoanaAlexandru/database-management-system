a772118b18fb95c76ac61b5c6bf7bc08d9a7faa9
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2019-02-19 00:32:13 +0200

    add INSERT INTO functionality

--- Database/Program.cs
+++ Database/Program.cs
@@ -13,18 +13,36 @@ namespace Database
         private const string TYPE_DATE = "DATETIME";
         private const string TYPE_DOUBLE = "REAL";
 
-        private readonly Dictionary<string, string> column_name_to_type;
-        private readonly List<string> lines;
+        private List<string> ColumnNames;
+        private Dictionary<string, string> ColumnNameToType;
+        private readonly List<string[]> Lines;
+        private int NrColumns = 0;
 
-        public Table(Dictionary<string, string> columns)
+        public Table(List<string> names, Dictionary<string, string> columns)
         {
-            this.column_name_to_type = columns;
+            ColumnNames = names;
+            ColumnNameToType = columns;
+            NrColumns = names.Count;
+        }
+        public List<string> GetColumnNames() { return ColumnNames; }
+
+        public void Insert(List<string> Names, List<string> Values)
+        {
+            var nameValuePairs = Names.Zip(Values, (n, v) => new { Name = n, Value = v });
+            string[] line = new string[NrColumns];
+            foreach(var pair in nameValuePairs)
+            {
+                line[ColumnNames.IndexOf(pair.Name)] = pair.Value;
+            }
+            Lines.Add(line);
         }
     }
 
     class Program
     {
         private const string CMD_CREATE_TABLE = "CREATE TABLE";
+        private const string CMD_INSERT_INTO = "INSERT INTO";
+        private const string CMD_VALUES = "VALUES";
         private const string CMD_QUIT = "QUIT";
         private const string CMD_EXIT = "EXIT";
 
@@ -33,28 +51,77 @@ namespace Database
             Dictionary<string, Table> tables = new Dictionary<string, Table>();
 
             string cmd = "";
+            int open_bracket, close_bracket;
             while (!cmd.Equals("QUIT") && !cmd.Equals("EXIT"))
             {
                 cmd = Console.ReadLine();
                 if (cmd.IndexOf(CMD_CREATE_TABLE) == 0)
                 {
                     cmd = cmd.Substring(cmd.IndexOf(CMD_CREATE_TABLE) + CMD_CREATE_TABLE.Length + 1);
+                    // Get substring up to the first space or bracket
                     string table_name = cmd.Substring(0, Math.Min(cmd.IndexOf(" "), cmd.IndexOf("(")));
 
-                    int open_bracket = cmd.IndexOf("(");
-                    int close_bracket = cmd.LastIndexOf(")");
+                    // Get column names and types
+                    open_bracket = cmd.IndexOf("(");
+                    close_bracket = cmd.LastIndexOf(")");
                     cmd = cmd.Substring(open_bracket + 1, close_bracket - open_bracket - 1);
-                    string[] columns = cmd.Substring(cmd.LastIndexOf(CMD_CREATE_TABLE) + 1).Split(',');
+                    string[] columns = cmd.Split(',');
 
                     Console.WriteLine("name = " + table_name);
+                    List<string> column_names = new List<string>();
                     Dictionary<string, string> column_params = new Dictionary<string, string>();
                     foreach (string s in columns)
                     {
                         string[] column_params_string = s.Trim().Split(' ');
+                        column_names.Add(column_params_string[0]);
                         column_params.Add(column_params_string[0], column_params_string[1].ToUpper());
                     }
 
-                    tables.Add(table_name, new Table(column_params));
+                    tables.Add(table_name, new Table(column_names, column_params));
+                }
+                else if (cmd.IndexOf(CMD_INSERT_INTO) == 0)
+                {
+                    cmd = cmd.Substring(cmd.IndexOf(CMD_INSERT_INTO) + CMD_INSERT_INTO.Length + 1);
+                    // Get substring up to the first space or bracket
+                    string table_name = cmd.Substring(0, Math.Min(cmd.IndexOf(" "), cmd.IndexOf("(")));
+
+                    Table table = tables[table_name];
+
+                    List<string> columns = new List<string>();
+                    List<string> values = new List<string>();
+
+                    if (cmd.IndexOf("(") < cmd.IndexOf(CMD_VALUES))
+                    {
+                        // Command specifies columns
+                        string aux_cmd = cmd.Substring(0, cmd.IndexOf(CMD_VALUES) - 1);
+                        open_bracket = aux_cmd.IndexOf("(");
+                        close_bracket = aux_cmd.LastIndexOf(")");
+                        aux_cmd = aux_cmd.Substring(open_bracket + 1, close_bracket - open_bracket - 1);
+
+                        string[] col_names = aux_cmd.Split(',');
+                        foreach (string c in col_names) {
+                            columns.Add(c.TrimStart().TrimEnd());
+                        }
+                    }
+                    else
+                    {
+                        // Command does not specify colums
+                        columns = table.GetColumnNames();
+                    }
+
+                    cmd = cmd.Substring(cmd.IndexOf(CMD_VALUES) + CMD_VALUES.Length + 1);
+
+                    open_bracket = cmd.IndexOf("(");
+                    close_bracket = cmd.LastIndexOf(")");
+                    cmd = cmd.Substring(open_bracket + 1, close_bracket - open_bracket - 1);
+
+                    string[] vals = cmd.Split(',');
+                    foreach (string v in vals)
+                    {
+                        values.Add(v.TrimStart().TrimEnd());
+                    }
+
+                    table.Insert(columns, values);
                 }
             }
         }
