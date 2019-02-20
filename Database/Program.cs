6bf822d52e5f293b74705b18e4365188957f9550
Ioana Alexandru <ioanaa.alexandru98@gmail.com>
2019-02-20 12:00:49 +0200

    style improvements

--- Database/Program.cs
+++ Database/Program.cs
@@ -1,63 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Database
 {
-    class Table
-    {
-        private const string TYPE_STRING = "VARCHAR";
-        private const string TYPE_BOOL = "BIT";
-        private const string TYPE_DATE = "DATETIME";
-        private const string TYPE_DOUBLE = "REAL";
-
-        private List<string> ColumnNames;
-        private readonly Dictionary<string, string> ColumnNameToType;
-        private readonly List<string[]> Lines = new List<string[]>();
-        private readonly int NrColumns = 0;
-
-        public Table(List<string> names, Dictionary<string, string> columns)
-        {
-            ColumnNames = names;
-            ColumnNameToType = columns;
-            NrColumns = names.Count;
-        }
-        public List<string> GetColumnNames() { return ColumnNames; }
-
-        public void Insert(List<string> Names, List<string> Values)
-        {
-            var nameValuePairs = Names.Zip(Values, (n, v) => new { Name = n, Value = v });
-            string[] line = new string[NrColumns];
-            foreach (var pair in nameValuePairs)
-            {
-                line[ColumnNames.IndexOf(pair.Name)] = pair.Value;
-            }
-            Lines.Add(line);
-        }
-
-        public List<string[]> Select(List<string> columns)
-        {
-            List<string[]> selectedLines = new List<string[]>();
-
-            foreach (var l in Lines)
-            {
-                string[] selectedColumns = new string[columns.Count];
-                int i = 0;
-
-                foreach (var c in columns)
-                {
-                    selectedColumns[i++] = l[ColumnNames.IndexOf(c)];
-                }
-
-                selectedLines.Add(selectedColumns);
-            }
-
-            return selectedLines;
-        }
-    }
-
     class Program
     {
         private const string CMD_CREATE_TABLE = "CREATE TABLE";
@@ -79,17 +24,19 @@ namespace Database
                 cmd = Console.ReadLine();
                 if (cmd.IndexOf(CMD_CREATE_TABLE) == 0)
                 {
+                    // Remove command name from string
                     cmd = cmd.Substring(cmd.IndexOf(CMD_CREATE_TABLE) + CMD_CREATE_TABLE.Length + 1);
+
                     // Get substring up to the first space or bracket
                     string table_name = cmd.Substring(0, Math.Min(cmd.IndexOf(" "), cmd.IndexOf("(")));
 
-                    // Get column names and types
+                    // Get column names and types from between brackets
                     open_bracket = cmd.IndexOf("(");
                     close_bracket = cmd.LastIndexOf(")");
                     cmd = cmd.Substring(open_bracket + 1, close_bracket - open_bracket - 1);
                     string[] columns = cmd.Split(',');
 
-                    Console.WriteLine("name = " + table_name);
+                    // Create lists for the table constructor
                     List<string> column_names = new List<string>();
                     Dictionary<string, string> column_params = new Dictionary<string, string>();
                     foreach (string s in columns)
@@ -103,10 +50,11 @@ namespace Database
                 }
                 else if (cmd.IndexOf(CMD_INSERT_INTO) == 0)
                 {
+                    // Remove command name from string
                     cmd = cmd.Substring(cmd.IndexOf(CMD_INSERT_INTO) + CMD_INSERT_INTO.Length + 1);
-                    // Get substring up to the first space or bracket
-                    string table_name = cmd.Substring(0, Math.Min(cmd.IndexOf(" "), cmd.IndexOf("(")));
 
+                    // Get substring up to the first space or bracket to obtain table name
+                    string table_name = cmd.Substring(0, Math.Min(cmd.IndexOf(" "), cmd.IndexOf("(")));
                     Table table = tables[table_name];
 
                     List<string> columns = new List<string>();
@@ -132,6 +80,7 @@ namespace Database
                         columns = table.GetColumnNames();
                     }
 
+                    // Get values from command
                     cmd = cmd.Substring(cmd.IndexOf(CMD_VALUES) + CMD_VALUES.Length + 1);
 
                     open_bracket = cmd.IndexOf("(");
@@ -148,21 +97,22 @@ namespace Database
                 }
                 else if (cmd.IndexOf(CMD_SELECT) == 0)
                 {
+                    // Get table name
                     string cmd_aux = cmd.Substring(cmd.IndexOf(CMD_FROM));
                     string table_name = cmd_aux.Split(' ')[1];
-
                     Table table = tables[table_name];
 
-                    cmd = cmd.Substring(cmd.IndexOf(CMD_SELECT) + CMD_SELECT.Length + 1);
-
-                    cmd = cmd.Substring(0, cmd.IndexOf(CMD_FROM) - 1).TrimEnd().TrimStart();
+                    // Get selected columns
                     List<string> columns = new List<string>();
 
-                    if (cmd.Equals("*"))
+                    cmd = cmd.Substring(cmd.IndexOf(CMD_SELECT) + CMD_SELECT.Length + 1);
+                    cmd = cmd.Substring(0, cmd.IndexOf(CMD_FROM) - 1).TrimEnd().TrimStart();
+
+                    if (cmd.Equals("*"))  // Select all columns
                     {
                         columns = table.GetColumnNames();
                     }
-                    else
+                    else  // Select specific columns
                     {
                         string[] col_names = cmd.Split(',');
                         foreach (string c in col_names)
@@ -173,6 +123,7 @@ namespace Database
 
                     var selected = table.Select(columns);
 
+                    // Print selected columns
                     foreach (var line in selected)
                     {
                         Console.Write("| ");
---
+++ Database/Table.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Database
+{
+    class Table
+    {
+        private const string TYPE_STRING = "VARCHAR";
+        private const string TYPE_BOOL = "BIT";
+        private const string TYPE_DATE = "DATETIME";
+        private const string TYPE_DOUBLE = "REAL";
+
+        private List<string> ColumnNames;
+        private readonly Dictionary<string, string> ColumnNameToType;
+        private readonly List<string[]> Lines = new List<string[]>();
+        private readonly int NrColumns = 0;
+
+        // Create table where 
+        public Table(List<string> names, Dictionary<string, string> columns)
+        {
+            ColumnNames = names;
+            ColumnNameToType = columns;
+            NrColumns = names.Count;
+        }
+        public List<string> GetColumnNames() { return ColumnNames; }
+
+        public void Insert(List<string> Names, List<string> Values)
+        {
+            var nameValuePairs = Names.Zip(Values, (n, v) => new { Name = n, Value = v });
+            string[] line = new string[NrColumns];
+            foreach (var pair in nameValuePairs)
+            {
+                line[ColumnNames.IndexOf(pair.Name)] = pair.Value;
+            }
+            Lines.Add(line);
+        }
+
+        public List<string[]> Select(List<string> columns, List<string[]> lines = null)
+        {
+            if (lines == null)
+            {
+                lines = Lines;
+            }
+            List<string[]> selectedLines = new List<string[]>();
+
+            foreach (var l in lines)
+            {
+                string[] selectedColumns = new string[columns.Count];
+                int i = 0;
+
+                foreach (var c in columns)
+                {
+                    selectedColumns[i++] = l[ColumnNames.IndexOf(c)];
+                }
+
+                selectedLines.Add(selectedColumns);
+            }
+
+            return selectedLines;
+        }
+    }
+}
